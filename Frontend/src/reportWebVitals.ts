f1f34f7a4cca39e781d04924b17f96221d56a327
Cheshta17 <122281116+Cheshta17@users.noreply.github.com>
2025-01-08 14:41:40 +0530

    Add files via upload

--- Frontend/src/reportWebVitals.ts
+++ Frontend/src/reportWebVitals.ts
@@ -1,6 +1,3 @@
-// src/reportWebVitals.ts
-
-// Import the specific functions directly from the respective modules
 import { onCLS as getCLS } from 'web-vitals/src/onCLS';
 import { onFID as getFID } from 'web-vitals/src/onFID';
 import { onFCP as getFCP } from 'web-vitals/src/onFCP';
@@ -9,7 +6,7 @@ import { onTTFB as getTTFB } from 'web-vitals/src/onTTFB';
 
 const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    getCLS(onPerfEntry);  // Callback will receive the metric data
+    getCLS(onPerfEntry);  
     getFID(onPerfEntry);
     getFCP(onPerfEntry);
     getLCP(onPerfEntry);
